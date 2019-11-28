fad42d5b8690ef47ce464fcd3e61eb314c8f29f1
thinkwhys <826886450@qq.com>
2019-11-28 22:37:08 +0800

    坚持 Vue home

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-item" v-for="(item,index) in recommends" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt />
+        <span>{{item.title}}</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.recommend {
+  display: flex;  /* 设置为flex盒子 */
+  width: 100%;
+  text-align: center;
+  font-size: 12px;
+
+  padding: 10px 0 20px;
+  border-bottom: 10px solid #eee;
+}
+
+.recommend-item {
+  flex: 1;  /* 均等分 */
+}
+
+.recommend-item img {
+  width: 70px;
+  height: 70px;
+  margin-bottom: 10px;
+}
+</style>
